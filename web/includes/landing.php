36cc422bce05872a5d6f6f86acc5d37845c46cc1
Vova <princessacelovekov@gmail.com>
2025-08-01 15:55:28 -0300

    не доработали видео

--- web/includes/landing.php
+++ web/includes/landing.php
@@ -1,9 +1,10 @@
 <div class="landing_video_block">
     <div class="landing_video_gradient"></div>
-    <video autoplay muted loop playsinline class="landing_video_bg">
-        <source src="videos/vid_digilang_mobile.mp4" type="video/mp4" media="(max-width: 768px)">
+    <video autoplay muted loop playsinline class="landing_video_bg_mobile">
+        <source src="videos/vid_digilang_mobile.mp4" type="video/mp4">
+    </video>
+    <video autoplay muted loop playsinline class="landing_video_bg_desktop">
         <source src="videos/vid_digilang.mp4" type="video/mp4">
-        Ваш браузер не поддерживает тег video.
     </video>
 
     <div class="landing_video_overlay">
