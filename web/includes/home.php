ae7b7299e6c873c2282da07107d819a331272b2c
lorlov <lorlov@ozon.ru>
2025-08-01 16:23:43 -0300

    Try to fix video for mobile

--- web/includes/home.php
+++
@@ -1,3 +0,0 @@
-<div class="logo_cont">
-  <img src="img/logo.png" class="logo_img">
-</div>
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digilang</title>
-    <link rel="stylesheet" href="css/style.css?v=0.0.1">
+    <link rel="stylesheet" href="css/style.css?t=<?=time()?>">
 </head>
 <body>
 
