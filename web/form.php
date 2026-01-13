f7ce0ecf4b1e5441387fd32eb0f0d901d1b58f23
cebem1nt <mineewarik@gmail.com>
2026-01-13 12:59:55 -0300

    Finished form

--- web/form.php
+++ web/form.php
@@ -14,11 +14,11 @@
    <div class="form_group">
         <div class="form_info">
             <h1 data-i18n="contacts">Contacts</h1>
-            <h2>WhatsApp</h2>
+            <a href="https://wa.me/541144724911"><h2>WhatsApp</h2></a>
             <h2>somemail@gmail.com</h2>
         </div>
         <form id="form_form">
-            <input type="text" id="form_name" data-i18n-placeholder="plh_text">
+            <input type="text" id="form_name" data-i18n-placeholder="plh_name">
             <input type="mail" id="form_mail" data-i18n-placeholder="plh_mail">
             <textarea name="message" id="form_message" data-i18n-placeholder="plh_message""></textarea>
             <button class="btn button_text" data-i18n="btn_send" style="cursor: pointer;" onclick=send(event)>Send</button>
@@ -31,10 +31,22 @@
 <script>
     function send(e) {
         e.preventDefault()
+        let url = "https://wa.me/541144724911?text="
 
-        console.log(form_name.value)
-        console.log(form_mail.value)
-        console.log(form_message.value)
+        const name = form_name.value
+        const mail = form_mail.value
+        const message = form_message.value
+
+        if (message)
+            url += message + '\n\n'
+        if (name && mail)
+            url += `${name} ${mail}`
+        else if (name)
+            url += name
+        else if (mail)
+            url += mail
+
+        window.location.href = encodeURI(url)
     }
 
 </script>
--- web/includes/landing_footer.php
+++ web/includes/landing_footer.php
@@ -1,11 +1,11 @@
 <div id="contacts" class="footer" data-i18n-scope="footer">
   <div class="footer_inner">
     <div class="footer_left">
-      <a class="footer_link" href="https://wa.me/XXXXXXXXXXX" target="_blank" rel="noopener" aria-label="WhatsApp">
+      <a class="footer_link" href="https://wa.me/541144724911" target="_blank" rel="noopener" aria-label="WhatsApp">
         <img src="img/whatsapp_icon.png" />
         <span>WhatsApp</span>
       </a>
-      <a class="footer_link" href="https://t.me/XXXXXXXXXXX" target="_blank" rel="noopener" aria-label="Telegram">
+      <a class="footer_link" href="https://t.me/digilang_pro" target="_blank" rel="noopener" aria-label="Telegram">
         <img src="img/telegram_icon.png" />
         <span>Telegram</span>
       </a>
@@ -17,17 +17,5 @@
         <span data-i18n="creative_lab">Креативная лаборатория</span>
       </div>
     </div>
-
-    <!-- <div class="footer_right">
-        <div class="footer_lang" onclick="toggleLangMenuFooter()">
-            <img src="img/landing_lang.png" />
-            <span id="current-lang-footer"><?= strtoupper($lang) ?></span>
-                <ul id="footer_lang_menu" class="footer_lang_menu hidden">
-                <li onclick="setLang('landing', 'es')">Español</li>
-                <li onclick="setLang('landing', 'en')">English</li>
-                <li onclick="setLang('landing', 'ru')">Русский</li>
-            </ul>
-        </div>
-    </div> -->
   </div>
 </div>
